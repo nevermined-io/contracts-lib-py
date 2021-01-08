c0610b823b5b05ee4dba78c7b499b2a8480d9043
enrique <enrique@keyko.io>
2021-01-08 14:55:26 +0100

    update changelog

--- setup.py
+++ setup.py
@@ -81,6 +81,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/contracts-lib-py',
-    version='0.5.3',
+    version='0.5.4',
     zip_safe=False,
 )
