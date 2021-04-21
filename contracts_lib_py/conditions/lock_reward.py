ff222dcc8eca36d4233ef5a4f867869011b92d64
Aitor <1726644+aaitor@users.noreply.github.com>
2021-04-21 10:02:33 +0200

    Adding `token_address` as new parameter to lock and escrow payment conditions

--- contracts_lib_py/conditions/lock_reward.py
+++ contracts_lib_py/conditions/lock_reward.py
@@ -1,26 +1,32 @@
 from contracts_lib_py.conditions.condition_base import ConditionBase
+from web3 import Web3
 
 
 class LockPaymentCondition(ConditionBase):
     """Class representing the LockPaymentCondition contract."""
     CONTRACT_NAME = 'LockPaymentCondition'
 
-    def fulfill(self, agreement_id, did, reward_address, amounts, receivers, account):
+    def fulfill(self, agreement_id, did, reward_address, token_address, amounts, receivers, account):
         """
         Fulfill the lock reward condition.
 
         :param agreement_id: id of the agreement, hex str
         :param did: id of the asset, hex str
         :param reward_address: the contract address where the reward is locked, hex str
+        :param token_address: the token address to use. If empty  it will use 0x0 address that means ETH payment
         :param amounts: Array of amount of tokens to distribute, int[]
         :param receivers: Array of ethereum address of the receivers, hex str[]
         :param account: Account instance
         :return:
         """
+        if not Web3.isAddress(token_address):
+            token_address = ConditionBase.ZERO_ADDRESS
+
         return self._fulfill(
             agreement_id,
             did,
             reward_address,
+            ConditionBase.validate_token_address(token_address),
             amounts,
             receivers,
             transact={'from': account.address,
@@ -28,19 +34,22 @@ class LockPaymentCondition(ConditionBase):
                       'keyfile': account.key_file}
         )
 
-    def hash_values(self, did, reward_address, amounts, receivers):
+    def hash_values(self, did, reward_address, token_address, amounts, receivers):
         """
         Hash of the values of the lock reward condition.
 
         :param did: id of the asset, hex str
         :param reward_address: the contract address where the reward is locked, hex str
+        :param token_address: the token address to use. If empty  it will use 0x0 address that means ETH payment
         :param amounts: Array of amount of tokens to distribute, int[]
         :param receivers: Array of ethereum address of the receivers, hex str[]
         :return: hex str
         """
+
         return self._hash_values(
             did,
             reward_address,
+            ConditionBase.validate_token_address(token_address),
             amounts,
             self.to_checksum_addresses(receivers)
         )
