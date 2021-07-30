85d149d51d6665aa7c127f569ba344e7c0f8cb9e
Rodolphe Marques <rodolphe@keyko.io>
2021-07-30 12:38:22 +0200

    bump version

--- setup.py
+++ setup.py
@@ -82,6 +82,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/contracts-lib-py',
-    version='0.7.5',
+    version='0.7.6',
     zip_safe=False,
 )
