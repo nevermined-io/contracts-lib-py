4c19e871588367c28924c03077f40de0dd99a7a5
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2021-09-22 09:23:44 +0200

    fix syntax

--- contracts_lib_py/keeper.py
+++ contracts_lib_py/keeper.py
@@ -55,8 +55,8 @@ class Keeper(object):
         44787: 'celo-alfajores',
         62320: 'celo-baklava',
         80001: 'mumbai',
-        1313161554: 'aurora-mainnet'
-        1313161555: 'aurora-testnet'
+        1313161554: 'aurora-mainnet',
+        1313161555: 'aurora-testnet',
         1313161556: 'aurora-betanet'
     }
 
