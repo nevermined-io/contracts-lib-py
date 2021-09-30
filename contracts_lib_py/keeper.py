5b6cf08e7b8dab6cdab2da2861843ae518ee031c
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2021-09-30 11:41:43 +0200

    fix aurora mainnet name

--- contracts_lib_py/keeper.py
+++ contracts_lib_py/keeper.py
@@ -57,7 +57,7 @@ class Keeper(object):
         44787: 'celo-alfajores',
         62320: 'celo-baklava',
         80001: 'mumbai',
-        1313161554: 'aurora-mainnet',
+        1313161554: 'aurora',
         1313161555: 'aurora-testnet',
         1313161556: 'aurora-betanet'
     }
