3f80d8d414d5ca6369043288e890f28d25b9185a
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2021-10-01 15:18:38 +0200

    v0.7.16

--- contracts_lib_py/__init__.py
+++ contracts_lib_py/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.7.15'
+__version__ = '0.7.16'
 from .contract_base import ContractBase
 from .didregistry import DIDRegistry
 from .dispenser import Dispenser
--- setup.py
+++ setup.py
@@ -82,6 +82,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/contracts-lib-py',
-    version='0.7.15',
+    version='0.7.16',
     zip_safe=False,
 )
