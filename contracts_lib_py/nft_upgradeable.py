7348c1a8d64c071f24fc2bec764656ee2272211a
Rodolphe Marques <rodolphe@keyko.io>
2021-12-02 10:44:09 +0100

    Added support for contracts 1.3.0
    - Fixed tests
    - bumped version to 0.9.2

--- contracts_lib_py/nft_upgradeable.py
+++ contracts_lib_py/nft_upgradeable.py
@@ -140,3 +140,27 @@ class NFTUpgradeable(ContractBase):
         )
         return self.is_tx_successful(tx_hash)
 
+    def balance(self, address, did):
+        """
+        Returns the NFT balance for an address
+
+        :param address: Account address to check, str
+        :param did: The NFT id, str
+        :return: int
+        """
+        return self.contract.caller.balanceOf(address, int(did, 16))
+
+    def transfer_nft(self, did, address, amount, account):
+        tx_hash = self.send_transaction(
+            'safeTransferFrom',
+            (account.address,
+             address,
+             int(did, 16),
+             amount,
+             Web3.toBytes(text='')
+             ),
+            transact={'from': account.address,
+                      'passphrase': account.password,
+                      'keyfile': account.key_file}
+        )
+        return self.is_tx_successful(tx_hash)
