20c2abe4a5b77d0a82c75cdd370220932668d6da
Sami Mäkelä <mrsmkl@gmail.com>
2022-01-13 12:29:57 +0200

    some changes

--- contracts_lib_py/wallet.py
+++ contracts_lib_py/wallet.py
@@ -63,11 +63,11 @@ class Wallet:
         # nonce = Wallet._get_nonce(self._web3, account.address)
         # logger.debug(f'`Wallet` signing tx: sender address: {account.address} nonce: {nonce}, '
         #              f'gasprice: {self._web3.eth.gasPrice}')
-        gas_price = self._web3.eth.max_priority_fee
-        # gas_price = self._web3.eth.gas_price
+        priority_fee = self._web3.eth.max_priority_fee
+        gas_price = self._web3.eth.gas_price
         tx['type'] = 2
         tx['nonce'] = self._web3.eth.getTransactionCount(account.address)
-        tx['maxPriorityFeePerGas'] = gas_price
+        tx['maxPriorityFeePerGas'] = priority_fee
         tx['maxFeePerGas'] = gas_price
         signed_tx = self._web3.eth.account.sign_transaction(tx, private_key)
         logger.debug(f'`Wallet` signed tx is {signed_tx}')
