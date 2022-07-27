4a9c0c332447147c7d94b3b2ef9695ab5480eef2
Aitor <1726644+aaitor@users.noreply.github.com>
2022-07-27 17:27:15 +0200

    Adding NFT 721 sales template

---
+++ contracts_lib_py/templates/nft721_sales_template.py
@@ -0,0 +1,10 @@
+import logging
+
+from contracts_lib_py.templates.template_base import TemplateBase
+
+logger = logging.getLogger('nft721SalesTemplate')
+
+
+class NFT721SalesTemplate(TemplateBase):
+    """Class representing the NFT721SalesTemplate contract."""
+    CONTRACT_NAME = 'NFT721SalesTemplate'
